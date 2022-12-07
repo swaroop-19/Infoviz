a7c0aca80ceb7765c0391c1b3ce96df6c1b64ce0
Sai Swaroop Nimmagadda <48797831+swaroop-19@users.noreply.github.com>
2022-12-06 21:04:13 -0500

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,7 +6,7 @@ const {tableau} = window;
 export default function Home() {
   const ref=useRef(null);
   const url=
-  "https://public.tableau.com/views/IVproject_16696906414440/overview-userinteraction?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link";
+  "https://public.tableau.com/views/IVfinalproject-4/overview-firstpage?:language=en-US&:display_count=n&:origin=viz_share_link";
   function initViz(){
     new tableau.Viz(ref.current,url);
   }
